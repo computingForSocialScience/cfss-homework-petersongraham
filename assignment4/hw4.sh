778ead3028b2171b8ca287b0e63b3bd69728f193
Graham Peterson <peterson_graham@wireless-s0-no-150-4-46.uchicago.edu>
2015-02-25 13:38:13 -0600

    changes

---
+++ assignment4/hw4.sh
@@ -0,0 +1,2 @@
+wc -1
+grep "Hyde Park" permits.csv > permits_hydepark.csv
--- assignment4/parse.py
+++ assignment4/parse.py
@@ -1,6 +1,9 @@
 import csv
 import sys
 
+print sys.argv[0]
+
+
 def readCSV(filename):
     '''Reads the CSV file `filename` and returns a list
     with as many items as the CSV has rows. Each list item 
@@ -10,7 +13,60 @@ def readCSV(filename):
     with open(filename,'r') as f:
         rdr = csv.reader(f)
         lines = list(rdr)
-    return(lines)
+	return(lines)
+
+hydepark = readCSV('permits_hydepark.csv')
+
+import math
+
+def get_avg_latlng(csvlist):
+	latitude = [item[128] for item in hydepark]
+	longitude = [item[129] for item in hydepark]
+	lat_list = []
+	long_list = []
+	
+	for i in range(len(latitude)) and range(len(longitude)):
+		lat_flt = float(latitude[i])
+		long_flt = float(longitude[i])
+		lat_list.append(lat_flt)
+		long_list.append(long_flt)
+	
+	avlat = sum(lat_list)/float(len(latitude))
+	avlong = sum(long_list)/float(len(longitude))
+		
+	print avlat, avlong
 
 
-### enter your code below
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+from matplotlib import pyplot as plt
+import Image
+
+def zip_code_barchart(sirmixalot):
+    zipcodes = []
+    for i in sirmixalot:
+    	if i[27] != "NJ":
+    		for column in (28, 35, 42, 49, 56, 63, 70, 84, 91, 98, 105, 112, 119, 126):
+    			if i[column] != '':
+    				zipcodes.append(i[column][0:5])
+
+    zipcodes_int = []
+    for code in zipcodes:
+    	zipcodes_int.append(int(code))
+
+    plt.hist(zipcodes_int, bins=200)
+    plt.xlabel('Contractor Zipcodes')
+    plt.ylabel('Frequency')
+    plt.title('Zipcode Histogram for City of Chicago Contractors')
+    plt.grid(True)
+    plt.draw()
+    plt.savefig('histogram.png')
+    Image.open('histogram.png').save('histogram.jpg', 'JPEG')
+
+if sys.argv[1] == 'latlong':
+    get_avg_latlng(hydepark)
+elif sys.argv[1] == 'hist':
+    zip_code_barchart(hydepark)
+
